22d943d9b2d5706672eeaf63d2dfe2bd34151d02
Rosie <rosie@riveter14-md.swna.wdpr.disney.com>
2016-05-02 15:56:07 -0700

    Updated Movie Class/table

--- model.py
+++ model.py
@@ -31,6 +31,12 @@ class Movie(db.Model):
     __tablename__ = "movies"
 
     movie_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    title = db.Column(db.String(64), nullable=False)
+    released_at = db.Column(db.DateTime, nullable=False)
+    imdb_url = db.Column(db.String(64), nullable=False)
+
+
+
 
 ##############################################################################
 # Helper functions
